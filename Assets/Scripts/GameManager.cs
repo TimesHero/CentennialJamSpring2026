be3d90feab36b27c1fc7542f36f1e87d7278f807
HoodedZombie <85358575+hoodedzombie@users.noreply.github.com>
2026-04-22 17:10:53 -0400

    Added portrait emotions and animations

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -7,20 +7,58 @@ public class GameManager : MonoBehaviour
 {
     [SerializeField] private DialogueRunner dialogueRunner;
     [SerializeField] private List<Character> characters;
+    [SerializeField] private SpriteRenderer portrait;
+    [SerializeField] private Animator animator;
+    private bool someoneLikes = false;
+    private Character pickedChar;
 
 
     [YarnCommand("GetCharacter")]
     public void GetCharacter()
     {
-        Character pickedChar = characters[UnityEngine.Random.Range(0, characters.Count)];
-        Debug.Log($"Picked {pickedChar.name}");
-        StartCoroutine(StartDialogue(pickedChar.name));
+        if(characters.Count <= 0)
+        {
+            if(someoneLikes) StartCoroutine(StartDialogue("GameClosingGood"));
+            else StartCoroutine(StartDialogue("GameClosingBad"));
+        }
+        else
+        {
+            int charIndex = UnityEngine.Random.Range(0, characters.Count);
+            pickedChar = characters[charIndex];
+            characters.RemoveAt(charIndex);
+            StartCoroutine(StartDialogue(pickedChar));
+            portrait.sprite = pickedChar.neutralSprite;
+        }
     }
 
-    //Added since ending a dialogue and starting a new one in the same frame skips lines
-    private IEnumerator StartDialogue(string charName)
+    [YarnCommand("SetLiked")]
+    public void WinCharacter()
     {
-        yield return new WaitForSeconds(0.25f);
-        dialogueRunner.StartDialogue($"{charName}DialogueStart");
+        someoneLikes = true;
+    }
+
+    private IEnumerator StartDialogue(Character character)
+    {
+        yield return new WaitForSeconds(0.25f); //Small delay to prevent skips
+        dialogueRunner.StartDialogue($"{character.name}DialogueStart");
+    }
+    private IEnumerator StartDialogue(string dialogueName) //Love me an overloaded function
+    {
+        yield return new WaitForSeconds(0.25f); //Small delay to prevent skips
+        dialogueRunner.StartDialogue(dialogueName);
+    }
+
+    [YarnCommand("SetPortraitSprite")]
+    public void SetPortraitSprite(string emotion)
+    {
+        if (emotion == "Happy") portrait.sprite = pickedChar.happySprite;
+        else if (emotion == "Neutral") portrait.sprite = pickedChar.neutralSprite;
+        else if (emotion == "Upset") portrait.sprite = pickedChar.upsetSprite;
+    }
+
+    [YarnCommand("TogglePortraitAnim")]
+    public void TogglePortraitAnim()
+    {
+        animator.SetBool("isVisible", !animator.GetBool("isVisible"));
     }
 }
--- Assets/Scripts/ScriptableObjects/Character.cs
+++ Assets/Scripts/ScriptableObjects/Character.cs
@@ -3,8 +3,9 @@ using UnityEngine;
 [CreateAssetMenu(fileName = "Character", menuName = "Scriptable Objects/Character")]
 public class Character : ScriptableObject
 {
-    public string name;
     public int minInterest;
     public int maxInterest;
     public Sprite neutralSprite;
+    public Sprite happySprite;
+    public Sprite upsetSprite;
 }
