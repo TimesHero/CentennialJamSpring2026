8b94d16c7d2757bba6082879466dc248606b4038
HoodedZombie <85358575+hoodedzombie@users.noreply.github.com>
2026-04-24 05:57:28 -0400

    Removed unused SO variables

--- Assets/Scripts/ScriptableObjects/Character.cs
+++ Assets/Scripts/ScriptableObjects/Character.cs
@@ -3,8 +3,6 @@ using UnityEngine;
 [CreateAssetMenu(fileName = "Character", menuName = "Scriptable Objects/Character")]
 public class Character : ScriptableObject
 {
-    public int minInterest;
-    public int maxInterest;
     public Sprite neutralSprite;
     public Sprite happySprite;
     public Sprite upsetSprite;
